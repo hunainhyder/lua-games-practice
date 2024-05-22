e02aa7116749fa5dc9f6c21d95c476e6346d6bf5
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2024-05-22 15:15:30 +0500

    rendered the bird

--- flappy-bird/main.lua
+++ flappy-bird/main.lua
@@ -1,7 +1,11 @@
 push = require 'push'
 
-WINDOW_WIDTH = 1080
-WINDOW_HEIGHT = 640
+Class = require 'class'
+
+require 'Bird'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
@@ -17,11 +21,13 @@ local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
 
+local bird = Bird()
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
+        fullscreen = true,
         resizable = true,
         vsync = true
     })
@@ -50,6 +56,8 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    bird:render()
 
     push:finish()
 end
